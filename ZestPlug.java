ce829861ff6019bd1fe56e3dd5599a2b2c1c90e7
aidan-ire <150788721+aidan-ire@users.noreply.github.com>
2026-02-18 16:16:46 +0000

    Update ZestPlug.java with a new implementation.

--- ZestPlug.java
+++ ZestPlug.java
@@ -1,4 +1,13 @@
+/**
+ * Concrete implementation of a German electrical plug connector.
+ * Represents a Zest brand German plug that can deliver electricity.
+ */
 public final class ZestPlug implements GermanPlugConnector {
+    
+    /**
+     * Delivers electricity through the Zest German plug connector.
+     * Prints a message to indicate the action.
+     */
     public void giveElectricity(){
         System.out.println("giving electricity to a zest plug.");
     }
