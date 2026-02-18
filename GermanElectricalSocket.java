b1d6b7032bc5a17398c005a498d234257ed5c117
aidan-ire <150788721+aidan-ire@users.noreply.github.com>
2026-02-18 16:17:10 +0000

    Updated GermanElectricalSocket.java to implement the adapter design pattern and handle German plug connectors.

--- GermanElectricalSocket.java
+++ GermanElectricalSocket.java
@@ -1,5 +1,15 @@
+/**
+ * Represents a German electrical socket that accepts German plug connectors.
+ * This class implements the target interface in the adapter design pattern,
+ * allowing German plug connectors to be plugged in and provide electricity.
+ */
 public class GermanElectricalSocket {
 
+    /**
+     * Plugs in a German plug connector and receives electricity from it.
+     * 
+     * @param plug the German plug connector to be plugged into this socket
+     */
     public void plugIn(GermanPlugConnector plug) {
         plug.giveElectricity();
     }
