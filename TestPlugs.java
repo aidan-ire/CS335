66269d91989f01ae3ce55bcf3f577c0ba0e31705
aidan-ire <150788721+aidan-ire@users.noreply.github.com>
2026-02-18 16:13:11 +0000

    Uploading lab2 files and javadocs

---
+++ TestPlugs.java
@@ -0,0 +1,37 @@
+public final class TestPlugs{
+
+	/** 
+	 * @param args[]
+	 */
+	public static void main (String args[]){
+		testGermanToUKAdapter();
+		testUKToGermanAdapter();
+	}
+	/*	
+	 *	Test Method for testing GermanToUK Adapter.
+	 */
+	public static void testGermanToUKAdapter(){
+		/* create a germany plug connector (brand: zest) */
+		GermanPlugConnector plug = new ZestPlug();
+		/* create a UK socket */
+		UKElectricalSocket socket = new UKElectricalSocket();
+		/* create an adapter */
+		UKPlugConnector ukAdapter = new GermanToUKPlugConnectorAdapter(plug);
+		/* use this adapter in a UK socket */
+		socket.plugIn(ukAdapter);
+	}
+
+	/*	
+	 *	Test Method for testing UKToGerman Adapter.
+	 */
+		public static void testUKToGermanAdapter(){
+			/* create a UK plug connector (brand: furutech) */
+			UKPlugConnector plug = new furutech();
+			/* create a german socket */
+			GermanElectricalSocket socket = new GermanElectricalSocket();
+			/* create an adapter */
+			GermanPlugConnector germanAdapter = new UKToGermanPlugConnectorAdapter(plug);
+			/* use this adapter in a german socket */
+			socket.plugIn(germanAdapter);
+	}
+}
---
+++ furutech.java
@@ -0,0 +1,5 @@
+public final class furutech implements UKPlugConnector{
+    public void provideElectricity(){
+        System.out.println("giving electricity to a furutech plug.");
+    }
+}
