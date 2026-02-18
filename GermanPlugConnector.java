faf36c0ef56a2c674865ccf5b5d29efd54359a28
aidan-ire <150788721+aidan-ire@users.noreply.github.com>
2026-02-18 16:18:33 +0000

    Update GermanPlugConnector.java with new interface definition

--- GermanPlugConnector.java
+++ GermanPlugConnector.java
@@ -1,4 +1,12 @@
+/**
+ * Interface for German electrical plug connectors.
+ * Defines the contract for all German plug connectors to provide electricity.
+ * This is the source interface in the adapter design pattern.
+ */
 public interface GermanPlugConnector {
 
+    /**
+     * Delivers electricity through this German plug connector.
+     */
     public void giveElectricity();
 }
