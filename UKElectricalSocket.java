cca5cf9f1ee81c445117f9865e7ec144271809e9
aidan-ire <150788721+aidan-ire@users.noreply.github.com>
2026-02-18 16:17:31 +0000

    Update UKElectricalSocket.java with new implementation

--- UKElectricalSocket.java
+++ UKElectricalSocket.java
@@ -1,5 +1,15 @@
+/**
+ * Represents a UK electrical socket that accepts UK plug connectors.
+ * This class implements the target interface in the adapter design pattern,
+ * allowing UK plug connectors to be plugged in and provide electricity.
+ */
 public class UKElectricalSocket {
 
+    /**
+     * Plugs in a UK plug connector and receives electricity from it.
+     * 
+     * @param plug the UK plug connector to be plugged into this socket
+     */
     public void plugIn(UKPlugConnector plug) {
         plug.provideElectricity();
     }
