8534cdd3fd0b496c7d461780503856d57e661e69
aidan-ire <150788721+aidan-ire@users.noreply.github.com>
2026-02-18 16:18:22 +0000

    Update UKPlugConnector.java interface definition

--- UKPlugConnector.java
+++ UKPlugConnector.java
@@ -1,4 +1,12 @@
+/**
+ * Interface for UK electrical plug connectors.
+ * Defines the contract for all UK plug connectors to provide electricity.
+ * This is the source interface in the adapter design pattern.
+ */
 public interface UKPlugConnector {
 
+    /**
+     * Delivers electricity through this UK plug connector.
+     */
     public void provideElectricity();
 }
