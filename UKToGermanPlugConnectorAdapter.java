c7c521c36d8d1eff24b511aaad8cb7a35feeeb30
aidan-ire <150788721+aidan-ire@users.noreply.github.com>
2026-02-18 16:16:54 +0000

    Update UKToGermanPlugConnectorAdapter.java with new content

--- UKToGermanPlugConnectorAdapter.java
+++ UKToGermanPlugConnectorAdapter.java
@@ -1,11 +1,28 @@
+/**
+ * Adapter that converts UK plug connectors to German plug connectors.
+ * This class implements the adapter design pattern, allowing UK plugs
+ * to work with German electrical sockets by adapting the interface.
+ */
 public class UKToGermanPlugConnectorAdapter implements GermanPlugConnector {
 
+    /**
+     * The UK plug connector being adapted.
+     */
     private UKPlugConnector plug;
 
+    /**
+     * Constructs an adapter with a UK plug connector.
+     * 
+     * @param plug the UK plug connector to be adapted
+     */
     public UKToGermanPlugConnectorAdapter(UKPlugConnector plug) {
         this.plug = plug;
     }
 
+    /**
+     * Gives electricity by delegating to the underlying UK plug connector.
+     * This method implements the German interface by calling the UK interface method.
+     */
     @Override
     public void giveElectricity() {
         plug.provideElectricity();
